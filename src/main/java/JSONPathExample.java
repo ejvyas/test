faab18292b5b49f5b82efa7713e7a604e0621cf2
Vyas S <>
2019-10-08 23:55:37 -0400

    Read file from disk Parse JSON file Example of JSONPath

---
+++ src/main/java/JSONPathExample.java
@@ -0,0 +1,68 @@
+/*
+ * Main$
+ *
+ * Copyright (c) 2019  Pegasystems Inc.
+ * All rights reserved.
+ *
+ * This  software  has  been  provided pursuant  to  a  License
+ * Agreement  containing  restrictions on  its  use.   The  software
+ * contains  valuable  trade secrets and proprietary information  of
+ * Pegasystems Inc and is protected by  federal   copyright law.  It
+ * may  not be copied,  modified,  translated or distributed in  any
+ * form or medium,  disclosed to third parties or used in any manner
+ * not provided for in  said  License Agreement except with  written
+ * authorization from Pegasystems Inc.
+ */
+
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class JSONPathExample {
+    public static void main(String args[]){
+        JSONPathExample main = new JSONPathExample();
+        String json = readLineByLineJava8(main.getFileFromResources("test.json").getPath());
+        ReadContext ctx = JsonPath.parse(json);
+        String result = ctx.read("$.stages[0].steps[0].name");
+        System.out.println(result);
+
+    }
+
+    private static String readLineByLineJava8(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
+    }
+
+    // get file from classpath, resources folder
+    private File getFileFromResources(String fileName) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
+
+    }
+}
